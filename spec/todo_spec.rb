4ca254060f1a8272ea310a720be29ce85ebaf297
terryhycheng <terryhycheng@gmail.com>
2023-01-09 15:42:58 +0000

    updated todo.rb & todo_spec.rb

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,51 +1,59 @@
 require_relative "../todo.rb"
 
 describe Todo do
-    before (:each) do
-        @Todo = Todo.new
+    before (:all) do
+        File.new("data_test.csv", "w")
+        @Todo = Todo.new("data_test.csv")
+    end
+
+    after (:all) do
+        File.delete("data_test.csv")
     end
 
     context ".list" do
-        it "lists out all to-dos" do
-            expect(@Todo.list).to be true
+        it "should display a correct message when the list is empty" do
+            expect{ @Todo.list }.to output("The list is empty.\n").to_stdout
         end
     end
 
     context ".add" do
-        it "displays a message with ID when a new to-do is successfully created" do
-            expect(@Todo.add).to be true
+        it "should display a message with ID when a new to-do is successfully created" do
+            @Todo.add("This is a test")
+            expect{ @Todo.list }.to output("1. This is a test\n").to_stdout
         end
 
-        it "throws an error when no user input is found" do
-            expect(@Todo.add).to be true
+        it "should throw an error when no user input is found" do
+            expect{ @Todo.add("") }.to output("Invalid input. Please enter again.\n").to_stdout
         end
     end
 
     context ".edit" do
-        it "updates the selected todo and return a message after successfully updated" do
-            expect(@Todo.edit).to be true
+        it "should update the selected todo with the new content" do
+            @Todo.edit(1,"This is an edited content")
+            expect{ @Todo.list }.to output("1. This is an edited content\n").to_stdout
         end
 
-        it "throws an error when user inputs a wrong to-do ID" do
-            expect(@Todo.edit).to be true
+        it "should throw an error when user inputs a wrong to-do ID" do
+            expect{ @Todo.edit("", "This is an edited content") }.to output("To-do is not found. Please enter again.\n").to_stdout
         end
 
-        it "throws an error when no user input is found" do
-            expect(@Todo.edit).to be true
+        it "should throw an error when user inputs empty content" do
+            expect{ @Todo.edit(1, "") }.to output("Invalid input. Please enter again.\n").to_stdout
         end
     end
 
     context ".delete" do
-        it "deletes a to-do and returns a message with the to-do ID" do
-            expect(@Todo.delete).to be true
+        it "should throw an error when user inputs a wrong to-do ID" do
+            expect{ @Todo.delete(3) }.to output("To-do is not found. Please enter again.\n").to_stdout
         end
 
-        it "throws an error when user inputs a wrong to-do ID" do
-            expect(@Todo.delete).to be true
+        it "should throw an error when user inputs an empty string" do
+            expect{ @Todo.delete("") }.to output("To-do is not found. Please enter again.\n").to_stdout
         end
 
-        it "throws an error when no user input is found" do
-            expect(@Todo.delete).to be true
+        it "should delete a to-do and returns a message with the to-do ID" do
+            @Todo.delete(1)
+            expect{ @Todo.list }.to output("The list is empty.\n").to_stdout
         end
     end
 end
--- todo.rb
+++ todo.rb
@@ -1,17 +1,83 @@
+require 'csv'
+
 class Todo
+    def initialize(file_name)
+        @file_name = file_name
+    end
+
+    # Print out all to-dos
     def list
-        true
+        if !load_todos.empty?
+            load_todos.each do |key, value|
+                puts "#{key}. #{value}"
+            end
+        else
+            puts "The list is empty."
+        end
     end
 
-    def add
-        true
+    # Take a string and add it to to-do list
+    def add(content)
+        if content && content != ""
+            list = load_todos
+            # Check if the list is empty
+            if list.length > 0
+                list[(list.to_a.last[0] + 1)] = content
+            else
+                list[1] = content
+            end
+            save_todos(list)
+        else
+            puts "Invalid input. Please enter again."
+        end
     end
 
-    def edit
-        true
+    # Take a todo ID and a new content then replace the old content of the todo with the new one
+    def edit(todo_id, content)
+        list = load_todos
+        if list.has_key?(todo_id)
+            if content && content != ""
+                list[todo_id] = content
+                save_todos(list)
+            else
+                puts "Invalid input. Please enter again."
+            end
+        else
+            puts "To-do is not found. Please enter again."
+        end
     end
 
-    def delete
-        true
+    # Take a todo ID and delete the todo with the same id
+    def delete(todo_id)
+        list = load_todos
+        if list.has_key?(todo_id)
+            list.delete_if do |key, value|
+                key == todo_id
+            end
+            save_todos(list)
+        else
+            puts "To-do is not found. Please enter again."
+        end
+    end
+
+    private
+
+    # Take an array and write each of its item into a CSV
+    def save_todos(list)
+        CSV.open(@file_name, "wb") do |csv|
+            list.each do |key, value|
+                csv << [key, value]
+            end
+        end
+    end
+
+    # Read data from a CSV and return an array
+    def load_todos
+        list = {}
+        CSV.foreach(@file_name) do |line|
+            id, content = line
+            list[id.to_i] = content
+        end
+        list
     end
 end
